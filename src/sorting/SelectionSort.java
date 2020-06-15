e10d3284ca9802cbe3d76d56427effa53948af68
asifali2011 <aliasif646@gmail.com>
2020-06-15 11:50:43 +0500

    selection sort algorithm implemented

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -2,4 +2,33 @@ package sorting;
 
 public class SelectionSort {
 
+	public static void main(String args[]) {
+
+		int numbers[] = { 14, 33, 27, 10, 35, 19, 25, 44 };
+
+		for (int i = 0; i < numbers.length; i++) {
+			int firstLoc = numbers[i];
+			int minimunNumber = numbers[i];
+			int minimumNumberIndex = 0;
+			for (int j = i; j < numbers.length; j++) {
+				if (minimunNumber > numbers[j]) {
+					minimunNumber = numbers[j];
+					minimumNumberIndex = j;
+
+				}
+
+			}
+
+			int temp = numbers[i];
+			numbers[i] = minimunNumber;
+			numbers[minimumNumberIndex] = temp;
+
+		}
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+
+	}
+
 }
